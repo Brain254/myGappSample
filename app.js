ba3b8467f8ae45ecd321b13aec9c4a8c5a1a4e6f
coder.ke <corelug@gmail.com>
2019-05-26 17:30:18 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,3 @@
+$(function(){
+	alert('Fisrt Git App');
+})
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First Git App</title>
+</head>
+<body>
+   <table>
+   	<thead>
+   		<tr>
+   			<th>Name</th>
+   			<th>Email</th>
+   			<th>CellPhone</th>
+   			<th>Location</th>
+   		</tr>
+   	</thead>
+   	<tbody>
+   		<tr>
+   			<td>Njuguna Mwaura</td>
+   			<td>xnjuguna@gmail.com</td>
+   			<td>+254725368866</td>
+   			<td>Kenya</td>
+   		</tr>
+   	</tbody>
+   </table>
+</body>
+</html>
